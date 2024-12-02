6d2772122ac426715d540716abe718a0fee036f8
Vinh-Dieu Lam <vinh@odyssey.systems>
2024-12-02 11:06:07 +0000

    remove cache check for now

--- crates/odyexp-desktop/assets/sw.js
+++ crates/odyexp-desktop/assets/sw.js
@@ -2,8 +2,16 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  // disable this for now, it needs to match our wasm and js file names
+  // currently trunk adds a random hash to the end of the file name
+  // so this won't work and gives 404 errors
+  // setting is in trunk.toml 
+  // [build]
+  // filehash = false // default is true
+
+  //
+  // './odyexp-desktop.js',
+  // './odyexp-desktop_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
